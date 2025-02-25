aa9ba6852704f8678f9f9b5deb67cb794fca22f4
denq113 <denjlii931@gmail.com>
2025-02-26 03:40:49 +0700

    hello4

---
+++ System_programing_pr_5/System_programing_pr_5/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace System_programing_pr_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.RichTextBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(44, 39);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(345, 149);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(44, 245);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(345, 149);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Данные для передачи в буфер обмена";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(282, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Данные для получения из буфера обмена";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 61);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Поместить данные в буфер обмена";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(492, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 58);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Получить одну запись из буфера обмена";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(492, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 57);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Отчистить буфер обмена";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(492, 258);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(192, 57);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Зашифровать данные в буфере";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(492, 337);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(192, 57);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Расшифровать данные из буфера";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtInput;
+        private System.Windows.Forms.RichTextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ System_programing_pr_5/System_programing_pr_5/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace System_programing_pr_5
+{
+    public partial class Form1 : Form
+    {
+        private const int Shift = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtInput.Text))
+            {
+                Clipboard.SetText(txtInput.Text);
+                MessageBox.Show("Данные скопированы в буфер обмена.");
+            }
+            else
+            {
+                MessageBox.Show("Поле ввода пустое!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                txtOutput.Text = Clipboard.GetText();
+            }
+            else
+            {
+                MessageBox.Show("Буфер обмена пуст!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            MessageBox.Show("Буфер обмена очищен.");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                string text = Clipboard.GetText();
+                string encryptedText = CaesarCipher(text, Shift);
+                Clipboard.SetText(encryptedText);
+                MessageBox.Show("Данные зашифрованы и помещены в буфер.");
+            }
+            else
+            {
+                MessageBox.Show("Буфер обмена пуст!");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                string encryptedText = Clipboard.GetText();
+                string decryptedText = CaesarCipher(encryptedText, -Shift);
+                txtOutput.Text = decryptedText;
+            }
+            else
+            {
+                MessageBox.Show("Буфер обмена пуст!");
+            }
+        }
+        private string CaesarCipher(string text, int shift)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (char ch in text)
+            {
+                if (char.IsLetter(ch))
+                {
+                    char baseChar = char.IsUpper(ch) ? 'A' : 'a';
+                    char encryptedChar = (char)(baseChar + (ch - baseChar + shift + 26) % 26);
+                    result.Append(encryptedChar);
+                }
+                else
+                {
+                    result.Append(ch); // Не шифруем знаки препинания и цифры
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
